9cf9da0b647eb26c5b281f0bb54bd75cf3c16a11
Gustavo Guimarães <gustavodossantosguimaraes93@gmail.com>
2026-04-08 09:40:42 -0300

    teste

--- src/main/java/com/example/teste1/Repository/LivroRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.teste1.Repository;
-
-import com.example.teste1.Models.LivroModels;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LivroRepository extends JpaRepository<LivroModels, Long> {
-}
